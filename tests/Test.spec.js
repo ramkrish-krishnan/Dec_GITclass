da3028577127f9a8fd21ea64e168523032b8aed7
ramkrish-krishnan <ramkrish201098@gmail.com>
2024-12-23 18:23:00 +0530

    updated Git command test files

---
+++ tests/Test.spec.js
@@ -0,0 +1,62 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  
+  await page.goto('https://www.saucedemo.com/v1/');
+
+  await page.waitForTimeout(2000)
+
+   await page.locator("//input[@name='user-name']").fill('standard_user');
+
+   await page.waitForTimeout(2000)
+ 
+   await page.locator("//input[@name='password']").fill('secret_sauce');
+
+   await page.waitForTimeout(2000)
+   
+   //await page.locator("input[@id='login-button']").click();
+
+   await page.click(('#login-button'));
+
+   await page.waitForTimeout(3000)
+
+   await page.click("(//button[text()='ADD TO CART'])[6]");
+
+   await page.waitForTimeout(2000)
+
+   await page.click("//div[@id='shopping_cart_container']");
+
+   await page.waitForTimeout(2000)
+
+   await page.click("//a[text()='CHECKOUT']");
+
+   await page.waitForTimeout(2000)
+
+   await page.fill("//input[@id='first-name']",'Ram')
+
+   await page.waitForTimeout(2000)
+
+   await page.fill("//input[@id='last-name']",'Krish')
+
+   await page.waitForTimeout(2000)
+
+   await page.fill("//input[@id='postal-code']",'60091')
+
+   await page.waitForTimeout(2000)
+
+   await page.click("//input[@value='CONTINUE']");
+
+   await page.waitForTimeout(2000)
+
+   const paymentinfo = await page.locator("//div[text()='SauceCard #31337']").textContent();
+   console.log(paymentinfo);
+
+   await page.waitForTimeout(2000)
+
+   await page.click("//a[text()='FINISH']");
+
+   await page.screenshot({path:'test-results/Screenshot/'+Date.now +'Result.png'});
+   
+   
+
+});
